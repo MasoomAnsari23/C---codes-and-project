8bdf6d207f09431a045f318e15dd6c54d6f206f7
masoomansari <masoom6ansari@gmail.com>
2024-03-15 14:56:43 +0530

    update on scoping rule.cpp file

--- functions/scopingrule.cpp
+++ functions/scopingrule.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 using namespace std;
-int g=9;
+int a=9;
 
 int main()
 {
     {
       int a=16;
       cout<<a<<endl;  /*scoping rule tells that the nearest variable will be accessed*/
-      cout<<g<<endl;
     }
  int a=23;
 cout<<a<<endl;
-cout<<g<<endl;
+cout<<::a<<endl;   /*this will access global variable instead of loacl variable*/
 return 0;
 }
