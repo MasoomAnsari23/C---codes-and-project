edb07fd57098c8c96b9f08bbd1d571a00d8a3e31
masoomansari <masoom6ansari@gmail.com>
2024-03-15 14:28:08 +0530

    global and local variable.cpp file

---
+++ functions/global&local variable.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int g=23;  /*global variable can be accsess by anywhere in program*/
+
+int fun()
+{
+    int a=10;
+    a++;/* this is local variable and only can be access within this funxn*/
+    g++;
+    cout<<a<<" "<<g<<endl;
+    return 0;
+}
+
+int main()
+{
+    cout<<g<<endl;
+    fun();
+    cout<<g<<endl;
+    return 0;
+}
