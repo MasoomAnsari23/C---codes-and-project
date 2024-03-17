80fdf259bb33a98ccc340f2cf0040069b460ef70
masoomansari <masoom6ansari@gmail.com>
2024-03-17 15:54:02 +0530

    sum of first n natural number.cpp file

---
+++ basics of c++.cpp/sumofNnaturalnos.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int sum;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    sum=(n*(n+1))/2;
+    cout<<"sum is "<<sum<<endl;
+    return 0;
+}
