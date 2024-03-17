a9d032a1f495a575dfc2568d9d11a1cd9e681423
masoomansari <masoom6ansari@gmail.com>
2024-03-17 15:23:18 +0530

    addition of 2 nos.cpp file

---
+++ basics of c++.cpp/add2nos.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int x,y,z;
+    cout<<"enter x and y"<<endl;
+    cin>>x>>y;
+    z=x+y;
+    cout<<"sum is"<<" "<<z;
+    return 0; 
+ }
