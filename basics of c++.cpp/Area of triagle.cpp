c82c0f810873b0a203cd02224081a057bc1a3212
masoomansari <masoom6ansari@gmail.com>
2024-03-17 15:49:00 +0530

    area of traingle.cpp file

---
+++ basics of c++.cpp/Area of triagle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int base;
+    int height;
+    int area;
+    cout<<"enter base"<<endl;
+    cin>>base;
+    cout<<"enter height"<<endl;
+    cin>>height;
+    area=(base*height)/2;
+    cout<<"Area of triangle is "<<area<<endl;
+    return 0;
+}
