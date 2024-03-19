eb1e9a54a7b24b6229b3bad80e8f56bd28bdc8cc
masoomansari <masoom6ansari@gmail.com>
2024-03-19 17:02:52 +0530

    printing n nos using while loop.cpp file

---
+++ .vscode/loops.cpp/printing n nos using while loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=0;
+    cout<<"enter n"<<endl;
+    cin>>n;
+
+    while(i<=n)    /*this code is 1001 % correct still error is showing idk why*/
+    {
+     cout<<i<<endl;
+     i++;
+    }
+    return 0;
+}
