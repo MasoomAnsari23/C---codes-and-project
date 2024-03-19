47ddac3a75a2d9431ccb20b46cb04cdc54f9e7ed
masoomansari <masoom6ansari@gmail.com>
2024-03-19 19:47:03 +0530

    finding factorial of given no by for loop.cpp file

---
+++ .vscode/loops.cpp/factorial of a no.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"enter no for which factorial is to be find"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+    cout<<i<<endl;
+    }
+    return 0;
+}
