e0b31af0ac3990175343a3a2a17654e742990b78
masoomansari <masoom6ansari@gmail.com>
2024-03-19 16:40:17 +0530

    printing n nos usong do while loop.cpp file

---
+++ .vscode/conditional statement.cpp/diplaying n nos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=1;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    do{
+        cout<<i<<endl;
+        i++;
+      }
+    while (i<=n);
+}
---
+++ .vscode/loops.cpp/printing n nos using do whileloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=0;
+    cout<<"enter n";
+    cin>>n;
+    do
+    {
+       cout<<i<<endl;
+       i++;
+    } 
+    while(i<=n);
+}
