d6ce3d34cf48d91e73383916c9747187a0287f6d
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-05-24 21:59:15 +0530

    more about preprocessor directives and namespace

---
+++ constant,preprocessor directives and namespace/more about preprocessor pedirective.cpp
@@ -0,0 +1,10 @@
+//finding maximum and square
+#include<iostream>
+using namespace std;
+
+#define max(x,y)  (x>y?x:y)
+#define sqr(x)  (x*x)
+int main(){
+  cout<<max(10,12)<<endl;
+  cout<<sqr(4);
+}
