d112435d40fbe754b5987d2ff2ea310c16253fa9
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-06-04 20:07:11 +0530

    variables

---
+++ revision of c++/basics/variables.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; //declaration of variable of type integer
+    a=10;  // initalization of variable
+
+    //also can be written as
+    // int a=10;
+     cout<<a<<endl;
+
+    char b='m';
+     cout<<b<<endl; 
+
+    float x=2.43f;
+     cout<<x<<endl; 
+
+    double y=3.23343;
+    cout<<y<<endl; 
+
+    bool h=4;
+    cout<<h<<endl;
+
+    string r="masoom";
+    cout<<r<<endl;
+
+
+}
