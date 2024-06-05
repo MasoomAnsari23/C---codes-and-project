cb1b165bba64ab2195849302518256a29cf0dea3
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-06-05 20:00:36 +0530

    redefining preprocessor directives

---
+++ constant,preprocessor directives and namespace/redefining preprocessor directives.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+#define  PI 3.145
+
+#define PI 3 //this is redifining PI and this redifne value hve higher priority
+
+int main(){
+    cout<<PI<<endl; // redefined PI will print
+}
