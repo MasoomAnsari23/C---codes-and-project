abaff308ad6f5861bee030262e25686268bfc0a3
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-06-05 19:59:49 +0530

    basic operation

---
+++ revision of c++/basics/basic operation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10,b=2;
+
+    cout<< "a+b ="<<a+b<<endl; //addition
+    cout<< "a*b ="<<a*b<<endl; //multiplication
+    cout<< "a-b ="<<a-b<<endl; //subtraction
+    cout<<"a%b ="<<a%b<<endl; //modules:gives remainder
+    cout<<"a/b ="<<a/b<<endl; //division
+}
