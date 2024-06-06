5cf4be1f0b92cfb688202ee4aa3ff096e50d6ced
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-06-06 21:31:18 +0530

    if-else example1

---
+++ revision of c++/consditional statement/if-else-1.cpp
@@ -0,0 +1,17 @@
+//printing greater number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a;
+    cin>>b;
+    if(a>b){
+        cout<<"greatest number is "<<a<<endl;
+    }
+    else{
+        cout<<"gretest number is "<<b<<endl;
+    }
+    return 0;
+}
