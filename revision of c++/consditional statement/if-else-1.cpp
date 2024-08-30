8cdabf58c8ad1d26cc5be8867deae01cdde3b09a
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-08-30 21:54:20 +0530

    adding one more code

--- revision of c++/consditional statement/if-else-1.cpp
+++ revision of c++/consditional statement/if-else-1.cpp
@@ -1,17 +1,37 @@
-//printing greater number
+// //printing greater number
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a, b;
+//     cin>>a;
+//     cin>>b;
+//     if(a>b){
+//         cout<<"greatest number is "<<a<<endl;
+//     }
+//     else{
+//         cout<<"gretest number is "<<b<<endl;
+//     }
+//     return 0;
+// }
 
 #include<iostream>
 using namespace std;
-
 int main(){
-    int a, b;
-    cin>>a;
-    cin>>b;
-    if(a>b){
-        cout<<"greatest number is "<<a<<endl;
+    int x,y,z;
+    cout<<"enter x,y and z"<<endl;
+    cin>>x>>y>>z;
+    if(x>y && x>z){
+        cout<<"greatest number is "<<x<<endl;
     }
+
+    else if(y>x && y>z){
+        cout<<"greatest number is "<<y<<endl;
+    }
+
     else{
-        cout<<"gretest number is "<<b<<endl;
+        cout<<"greatest number is "<<z<<endl;
     }
     return 0;
 }
