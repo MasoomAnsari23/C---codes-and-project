5ff062718055ea046c5c066e5ede46fb297da9ec
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-08-31 21:55:55 +0530

    printing n number using for loop

---
+++ revision of c++/lopps/for Loop.cpp
@@ -0,0 +1,14 @@
+// displaying n number using for loop
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"enter n"<<endl;
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        cout<<i<<endl;
+    }  // in for loop avoid return 0;
+}
