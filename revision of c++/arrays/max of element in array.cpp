7eee4b66e14dbaa0b6ea55dd34f70d721467b327
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-09 07:32:48 +0530

    finding maximum of element in array

---
+++ revision of c++/arrays/max of element in array.cpp
@@ -0,0 +1,16 @@
+//finding max of element in given array
+#include<iostream>
+using namespace std;
+int main(){
+    int n=6,max;
+    int i=0;
+    int A[6]={2,6,4,3,7,9};
+    max=A[0]
+    for(int i=1;i<n;i++){
+    if(A[i]>max){
+     max=A[i];
+    }
+    cout<<"max number is "<<max<<endl;
+    }
+    return 0;
+    }
