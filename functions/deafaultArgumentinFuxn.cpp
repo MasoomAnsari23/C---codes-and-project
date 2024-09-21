cf8f1e47e8272143c12036b34a12dec59047124d
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-09-21 21:42:40 +0530

    default argument

--- functions/deafaultArgumentinFuxn.cpp
+++ functions/deafaultArgumentinFuxn.cpp
@@ -16,10 +16,34 @@ int main()
     int d;        /*both of this will work*/
     d=max(22,45,21);
     cout<<d<<endl;
-    return 0;
 
-    /*int x=5,y=6,z=8,d;
-    d=max(x,y,z);
-    cout<<d<<endl;
-    return 0;*/
+
+    int x=5,y=6,z=8;
+    int e;
+    e=max(x,y,z);
+    cout<<e<<endl;
+    return 0;
+}
+
+//by default argument taking input from user
+#include<iostream>
+using namespace std;
+
+int max(int a,int b,int c=0){
+    if(a>b && a>c){
+        return a;
+    }
+    else if(b>a && b>c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}
+int main(){
+    int x,y,z,m;
+    cout<<"enter value of x,y,z";
+    cin>>x>>y>>z;
+    m=max(x,y,z);
+    cout<<"greatest number is "<<m<<endl;
 }
